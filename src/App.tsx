b773b52fe83e0a25c5b6e817ddd416785967ceb3
파링 <parangee9706@gmail.com>
2022-01-27 23:57:56 +0900

    세팅 탭 시스템

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,65 @@
 import React from 'react'
+import { Box, CssBaseline, Paper, Tab, Tabs } from '@mui/material'
+import TextCommandSettings from './ConfigSections/TextCommandSettings'
+
+type ConfigTab = {
+  label: string
+  component: React.ComponentType
+}
+
+const ConfigTabs: ConfigTab[] = [
+  {
+    label: '텍스트 설정 커맨드',
+    component: TextCommandSettings,
+  },
+]
 
 const App: React.FC = () => {
-  return <div>app</div>
+  const [currentTab, setCurrentTab] = React.useState(0)
+
+  return (
+    <CssBaseline>
+      <Box
+        sx={{
+          minHeight: '100vh',
+          p: 2,
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <Paper
+          sx={{
+            maxWidth: 500,
+            width: '100%',
+            mx: 'auto',
+            my: 'auto',
+            overflow: 'hidden',
+          }}
+          variant="outlined"
+        >
+          <Tabs
+            variant="scrollable"
+            scrollButtons="auto"
+            onChange={(e, v) => setCurrentTab(v)}
+            value={currentTab}
+          >
+            {ConfigTabs.map((x, i) => (
+              <Tab label={x.label} key={i} value={i} />
+            ))}
+          </Tabs>
+          <Box sx={{ p: 2 }}>
+            {ConfigTabs.map((x, i) =>
+              i === currentTab ? (
+                <div key={i}>
+                  <x.component />
+                </div>
+              ) : null
+            )}
+          </Box>
+        </Paper>
+      </Box>
+    </CssBaseline>
+  )
 }
 
 export default App
---
+++ src/ConfigSections/TextCommandSettings.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TextCommandSettings: React.FC = () => {
+  return <div>asdf</div>
+}
+
+export default TextCommandSettings
